95270f4db8262034d43ceed01da7f04bdaf0da1d
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-07 18:00:33 -0400

    Added README.md

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
 (defproject simple-emailer "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.apache.commons/commons-email "1.2"]]
+
+  :repositories [["central-proxy" "http://repository.sonatype.org/content/repositories/central/"]]
+
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+		     [marginalia "0.6.0"]])
--- src/simple_emailer/core.clj
+++ src/simple_emailer/core.clj
@@ -1,1 +1,12 @@
-(ns simple-emailer.core)
+(ns simple-emailer.core
+  (:use simple-emailer.helper))
+
+(defn send-email [params]
+  (let [session (create-session params)
+        message (create-message session params)]
+    (deliver-message message)))
+
+(defn create-mailer [params]
+  (let [session (create-session params)]
+    (fn [details]
+      (deliver-message (create-message session (merge params details))))))
