dd0d9f220b06d014fc1384ee7e91c96095bb2665
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-11-27 12:21:32 -0800

    Updated to 1.3.0

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject simple-emailer "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.apache.commons/commons-email "1.2"]]
 
   :repositories [["central-proxy" "http://repository.sonatype.org/content/repositories/central/"]]
 
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-		     [marginalia "0.6.0"]])
+  :dev-dependencies [[swank-clojure "1.3.0"
+                      :exclusions [org.clojure/clojure
+                                   org.clojure/clojure-contrib]]
+                     [marginalia "0.7.0-SNAPSHOT"]])
