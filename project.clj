62c8e107a723bef6548805ca7cf82e4ec9e73a22
Corey M. Hoffstein <corey@newfoundresearch.com>
2012-03-01 16:07:19 -0500

    Added repos to project file

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.apache.commons/commons-email "1.2"]]
 
-  :repositories [["central-proxy" "http://repository.sonatype.org/content/repositories/central/"]]
+  :dev-dependencies [[marginalia "0.7.0-SNAPSHOT"]]
 
-  :dev-dependencies [[swank-clojure "1.3.0"
-                      :exclusions [org.clojure/clojure
-                                   org.clojure/clojure-contrib]]
-                     [marginalia "0.7.0-SNAPSHOT"]])
+  :plugins [[s3-wagon-private "1.1.1"]]
+
+  :repositories {"nfr-releases" "s3p://newfound-mvn-repo/releases/"
+                 "nfr-snapshots" "s3p://newfound-mvn-repo/snapshots/"
+                 "central-proxy" "http://repository.sonatype.org/content/repositories/central/"})
